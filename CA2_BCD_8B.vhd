022590ff161bd0c50720f39d58e11b9c282e7a4c
Xavier Mendez <me@jmendeth.com>
2016-11-04 16:26:09 +0100

    Lab 2 (2016-11-03)

---
+++ CA2_BCD_8B.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_signed.all;
+
+entity CA2_BCD_8B is
+  port ( Ca2 : in std_logic_vector(4 downto 0);
+         BCD : out std_logic_vector(7 downto 0) );
+end;
+
+architecture logic of CA2_BCD_8B is
+begin
+end logic;
