052559501edf4c2046f32a9d4358216c2bb89a41
Xavier Mendez <me@jmendeth.com>
2016-11-17 11:48:56 +0100

    Lab 2016-11-15: Show selop value in board LEDs

--- leds.vhd
+++ leds.vhd
@@ -3,11 +3,17 @@ use ieee.std_logic_1164.all;
 
 entity leds is
   port ( show : in std_logic;
-         LED_GREEN, LED_RED : out std_logic_vector(3 downto 0) );
+         selop : in std_logic_vector(1 downto 0);
+         LED_OP, LED_RED, LED_GREEN : out std_logic_vector(3 downto 0) );
 end;
 
 architecture logic of leds is
 begin
-  LED_GREEN <= "1111" when show = '1' else "0000";
+  LED_OP(3) <= '1' when selop = "00" else '0';
+  LED_OP(2) <= '1' when selop = "01" else '0';
+  LED_OP(1) <= '1' when selop = "10" else '0';
+  LED_OP(0) <= '1' when selop = "11" else '0';
+  
   LED_RED <= "1111" when show = '0' else "0000";
+  LED_GREEN <= "1111" when show = '1' else "0000";
 end logic;
