e520bc72e66845362de755fffccc9b636e192344
Xavier Mendez <me@jmendeth.com>
2016-11-29 14:10:14 +0100

    Extend comptador, registres for 12-bit BCD

--- joc/comptador.vhd
+++ joc/comptador.vhd
@@ -2,34 +2,49 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+entity comptador_single is
+  port ( ecnt : in std_logic;
+         nrst, clk : in std_logic;
+         numx : out std_logic_vector(3 downto 0);
+         tc : out std_logic );
+end;
+
+architecture logic of comptador is
+  signal n : unsigned(3 downto 0);
+begin
+  process (clk, nrst)
+  begin
+    if nrst = '0' then
+      n <= x"0";
+    elsif (clk'event and clk = '1') then
+      if ecnt = '1' then
+        n <= x"0" when n = 9 else n + 1;
+      end if;
+    end if;
+  end process;
+
+  numx <= std_logic_vector(n);
+  tc <= '1' when (ecnt = '1' and n = 9) else '0';
+end logic;
+
 entity comptador is
   port ( ecnt : in std_logic;
          nrst, clk : in std_logic;
          numx : out std_logic_vector(7 downto 0) );
 end;
 
-architecture logic of comptador is
-  signal n1 : unsigned(3 downto 0);
-  signal n0 : unsigned(3 downto 0);
+architecture components of comptador is
+  component comptador_single is
+    port ( ecnt : in std_logic;
+           nrst, clk : in std_logic;
+           num : out std_logic_vector(3 downto 0);
+           tc : out std_logic );
+  end;
+
+  signal tc : std_logic_vector(3 downto 0);
 begin
-  process (clk, nrst)
-  begin
-    if nrst = '0' then
-      n1 <= x"0";
-      n0 <= x"0";
-    elsif (clk'event and clk = '1') then
-
-      if ecnt = '1' then
-        if n0 = 9 then
-          if n1 = 9 then n1 <= x"0"; else n1 <= n1 + 1; end if;
-          n0 <= x"0";
-        else
-          n0 <= n0 + 1;
-        end if;
-      end if;
-
-    end if;
-  end process;
-
-  numx <= std_logic_vector(n1) & std_logic_vector(n0);
-end logic;
+  tc(0) <= ecnt;
+  comptador_0 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(0), tc => tc(1), numx => numx(3 downto 0));
+  comptador_1 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(1), tc => tc(2), numx => numx(7 downto 4));
+  comptador_2 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(2), tc => tc(3), numx => numx(11 downto 8));
+end components;
--- joc/registres.vhd
+++ joc/registres.vhd
@@ -6,24 +6,26 @@ entity registres is
          nrst : in std_logic;
          eshft : in std_logic;
          keycode : in std_logic_vector(3 downto 0);
-         num : out std_logic_vector(7 downto 0) );
+         num : out std_logic_vector(11 downto 0) );
 end;
 
 architecture logic of registres is
-  signal a, b : std_logic_vector(3 downto 0);
+  signal a, b, c : std_logic_vector(3 downto 0);
 begin
   process (clk,nrst)
   begin
   if nrst = '0' then
     a <= "0000";
     b <= "0000";
+    c <= "0000";
   elsif rising_edge(clk) then
     if eshft = '1' then
+      c <= b;
       b <= a;
       a <= keycode;
     end if;
   end if;
   end process;
   
-  num <= b & a;
+  num <= c & b & a;
 end logic;
