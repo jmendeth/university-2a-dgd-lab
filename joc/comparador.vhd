6ffd95b6c4451f041778baa3eddc703fa68060a7
Xavier Mendez <me@jmendeth.com>
2016-12-11 11:31:10 +0100

    Finish extra2

--- joc/comparador.vhd
+++ joc/comparador.vhd
@@ -3,17 +3,38 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity comparador is
-  port ( numx, num : in std_logic_vector(7 downto 0);
+  port ( numx, num : in std_logic_vector(11 downto 0);
          ngtx, neqx, nltx : out std_logic );
 end;
 
-architecture logic of comparador is
-  signal a1, a0, b1, b0 : std_logic_vector(3 downto 0);
-begin
-  a1 <= num(7 downto 4); a0 <= num(3 downto 0);
-  b1 <= numx(7 downto 4); b0 <= numx(3 downto 0);
+architecture components of comparador is
+  component comparador_single is
+    port ( numx, num : in std_logic_vector(3 downto 0);
+           ngtxi, neqxi, nltxi : in std_logic;
+           ngtx, neqx, nltx : out std_logic );
+  end component;
 
-  ngtx <= '1' when (a1 > b1 or (a1 = b1 and a0 > b0)) else '0';
-  nltx <= '1' when (a1 < b1 or (a1 = b1 and a0 < b0)) else '0';
-  neqx <= '1' when numx = num else '0';
-end logic;
+  signal ngtxi : std_logic_vector(3 downto 0);
+  signal neqxi : std_logic_vector(3 downto 0);
+  signal nltxi : std_logic_vector(3 downto 0);
+begin
+  ngtxi(3) <= '0'; neqxi(3) <= '1'; nltxi(3) <= '0';
+  
+  comparador_2: comparador_single port map(
+    ngtxi => ngtxi(3), neqxi => neqxi(3), nltxi => nltxi(3),
+    numx => numx(11 downto 8), num => num(11 downto 8),
+    ngtx => ngtxi(2), neqx => neqxi(2), nltx => nltxi(2)
+  );
+  comparador_1: comparador_single port map(
+    ngtxi => ngtxi(2), neqxi => neqxi(2), nltxi => nltxi(2),
+    numx => numx(7 downto 4), num => num(7 downto 4),
+    ngtx => ngtxi(1), neqx => neqxi(1), nltx => nltxi(1)
+  );
+  comparador_0: comparador_single port map(
+    ngtxi => ngtxi(1), neqxi => neqxi(1), nltxi => nltxi(1),
+    numx => numx(3 downto 0), num => num(3 downto 0),
+    ngtx => ngtxi(0), neqx => neqxi(0), nltx => nltxi(0)
+  );
+  
+  ngtx <= ngtxi(0); neqx <= neqxi(0); nltx <= nltxi(0);
+end components;
--- joc/comptador.vhd
+++ joc/comptador.vhd
@@ -1,50 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity comptador_single is
-  port ( ecnt : in std_logic;
-         nrst, clk : in std_logic;
-         numx : out std_logic_vector(3 downto 0);
-         tc : out std_logic );
-end;
-
-architecture logic of comptador is
-  signal n : unsigned(3 downto 0);
-begin
-  process (clk, nrst)
-  begin
-    if nrst = '0' then
-      n <= x"0";
-    elsif (clk'event and clk = '1') then
-      if ecnt = '1' then
-        n <= x"0" when n = 9 else n + 1;
-      end if;
-    end if;
-  end process;
-
-  numx <= std_logic_vector(n);
-  tc <= '1' when (ecnt = '1' and n = 9) else '0';
-end logic;
 
 entity comptador is
   port ( ecnt : in std_logic;
          nrst, clk : in std_logic;
-         numx : out std_logic_vector(7 downto 0) );
+         numx : out std_logic_vector(11 downto 0) );
 end;
 
 architecture components of comptador is
   component comptador_single is
     port ( ecnt : in std_logic;
            nrst, clk : in std_logic;
-           num : out std_logic_vector(3 downto 0);
+           numx : out std_logic_vector(3 downto 0);
            tc : out std_logic );
-  end;
+  end component;
 
   signal tc : std_logic_vector(3 downto 0);
 begin
   tc(0) <= ecnt;
-  comptador_0 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(0), tc => tc(1), numx => numx(3 downto 0));
-  comptador_1 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(1), tc => tc(2), numx => numx(7 downto 4));
-  comptador_2 : comptador port map(clk => clk, nrst => nrst, ecnt => tc(2), tc => tc(3), numx => numx(11 downto 8));
+  comptador_0 : comptador_single port map(clk => clk, nrst => nrst, ecnt => tc(0), tc => tc(1), numx => numx(3 downto 0));
+  comptador_1 : comptador_single port map(clk => clk, nrst => nrst, ecnt => tc(1), tc => tc(2), numx => numx(7 downto 4));
+  comptador_2 : comptador_single port map(clk => clk, nrst => nrst, ecnt => tc(2), tc => tc(3), numx => numx(11 downto 8));
 end components;
